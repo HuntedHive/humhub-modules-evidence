aebc4f05fb9d225f05f0ab273a0398363e31a391
Bec Todd <bec@bectodd.com>
2016-05-14 07:24:48 +1000

    TC-25 Evidence Gathering Module
    - UI updates for evidence gathering module Step 1
    - add assets/css folder

--- views/evidence/index.php
+++ views/evidence/index.php
@@ -1,21 +1,40 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default qanda-panel">
-            <div class="panel-body">
+<link rel="stylesheet" type="text/css"
+         href="<?php echo $this->module->assetsUrl; ?>/css/evidence.css"/>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h4>
+                <strong>Export</strong> my evidence
+            </h4>
+            <p><strong>Step 1 of 3</strong> - Select the data you would like to export and download to .doc format.</p>
+        </div>
+     </div>
+     <div class="row">
+        <div class="col-xs-6">
+            <a class="btn btn-primary" href="#">Date picker</a>
+        </div>
+        <div class="col-xs-6 text-right">
+            <a class="btn btn-primary" href="#">Next Step: Context</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="table-responsive">
                 <?php
                 $this->widget('zii.widgets.grid.CGridView', array(
                     'dataProvider'=>$dataProvider,
                     'id'=>'customDataList',
                     'columns' => array(
                         array(
-                            'name' => 'select',
+                            'name' => 'Select',
                             'type' => 'raw',
                             'value' => function($data) {
                                 return '<input type="checkbox" data-id="' . $data['id'] . '">';
                             },
                         ),
                         array(
-                            'name' => 'Activity date',
+                            'name' => 'Activity Date',
                             'value' => function($data) {
                                 return $data['created_at'];
                             },
@@ -27,13 +46,13 @@
                             },
                         ),
                         array(
-                            'name' => 'Text(First xxx)',
+                            'name' => 'Text (First xxx)',
                             'value' => function($data) {
                                 return Evidence::getText($data);
                             },
                         ),
                         array(
-                            'name' => 'Target/recipient',
+                            'name' => 'Target/Recipient',
                             'value' => function($data) {
                                 return Evidence::getTarget($data);
                             },
@@ -42,6 +61,12 @@
                 ));
                 ?>
             </div>
+        </div>
+        <br>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 text-right">
+            <a class="btn btn-primary" href="#">Next Step: Context</a>
         </div>
     </div>
 </div>
--- widgets/views/evidenceView.php
+++ widgets/views/evidenceView.php
@@ -1,12 +1,12 @@
 <?php
-
 if(Yii::app()->controller->id == "profile") {
 ?>
+
     <script>
         $(document).ready(function() {
             var linkUrl = '<?= Yii::app()->createUrl("evidence/evidence/prepare"); ?>';
             console.log(linkUrl);
-            var linkButton = "<a class='btn btn-primary' href='" + linkUrl + "'>export my evidence</a>";
+            var linkButton = "<a class='btn btn-primary' href='" + linkUrl + "'><i class='fa fa-download'></i> Export my evidence</a>&nbsp;";
 
             $(".controls-header a").before(linkButton);
         })
