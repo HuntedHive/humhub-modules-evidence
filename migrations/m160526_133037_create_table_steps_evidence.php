e7dee855acabf9e696068f60f2fd0039641b483d
Den Volkov <volkovdenya@gmail.com>
2016-08-12 09:48:59 +0300

    add exists create of table

--- migrations/m160526_133037_create_table_steps_evidence.php
+++ migrations/m160526_133037_create_table_steps_evidence.php
@@ -6,20 +6,21 @@ class m160526_133037_create_table_steps_evidence extends Migration
 {
 	public function up()
 	{
-		$this->createTable('current_step_evidence', array(
-			'id' => 'pk',
-			'step1' => 'text NULL',
-			'step2' => 'text NULL',
-			'step3' => 'text NULL',
-			'obj_step1' => 'text NULL',
-			'obj_step2' => 'text NULL',
-			'obj_step3' => 'text NULL',
-			'created_at' => 'datetime NOT NULL',
-			'created_by' => 'int(11) NOT NULL',
-			'updated_at' => 'datetime NOT NULL',
-			'updated_by' => 'int(11) NOT NULL',
-		), '');
-
+		if(!\Yii::$app->db->schema->getTableSchema("current_step_evidence")) {
+			$this->createTable('current_step_evidence', array(
+				'id' => 'pk',
+				'step1' => 'text NULL',
+				'step2' => 'text NULL',
+				'step3' => 'text NULL',
+				'obj_step1' => 'text NULL',
+				'obj_step2' => 'text NULL',
+				'obj_step3' => 'text NULL',
+				'created_at' => 'datetime NOT NULL',
+				'created_by' => 'int(11) NOT NULL',
+				'updated_at' => 'datetime NOT NULL',
+				'updated_by' => 'int(11) NOT NULL',
+			), '');
+		}
 	}
 
 	public function down()
--- migrations/m160526_133040_create_save_step_evidence.php
+++ migrations/m160526_133040_create_save_step_evidence.php
@@ -6,21 +6,22 @@ class m160526_133040_create_save_step_evidence extends Migration
 {
 	public function up()
 	{
-		$this->createTable('save_steps_evidence', array(
-			'id' => 'pk',
-			'name' => 'varchar(100) NOT NULL',
-			'step1' => 'text NULL',
-			'step2' => 'text NULL',
-			'step3' => 'text NULL',
-			'obj_step1' => 'text NULL',
-			'obj_step2' => 'text NULL',
-			'obj_step3' => 'text NULL',
-			'created_at' => 'datetime NOT NULL',
-			'created_by' => 'int(11) NOT NULL',
-			'updated_at' => 'datetime NOT NULL',
-			'updated_by' => 'int(11) NOT NULL',
-		), '');
-
+		if(!\Yii::$app->db->schema->getTableSchema("save_steps_evidence")) {
+			$this->createTable('save_steps_evidence', array(
+				'id' => 'pk',
+				'name' => 'varchar(100) NOT NULL',
+				'step1' => 'text NULL',
+				'step2' => 'text NULL',
+				'step3' => 'text NULL',
+				'obj_step1' => 'text NULL',
+				'obj_step2' => 'text NULL',
+				'obj_step3' => 'text NULL',
+				'created_at' => 'datetime NOT NULL',
+				'created_by' => 'int(11) NOT NULL',
+				'updated_at' => 'datetime NOT NULL',
+				'updated_by' => 'int(11) NOT NULL',
+			), '');
+		}
 	}
 
 	public function down()
